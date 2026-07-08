55fa6d27e823676d3adce1fb25df00ab82eb8d2d
Iqra-hussain235 <iqratechnical@gmail.com>
2026-07-08 23:12:48 +0530

    Add feature from

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,7 +6,6 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
-  
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
